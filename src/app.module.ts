fa3d9dd15958289ddcb35303aeddc24ac746581a
HaroldSantos10 <haroldsan9.9@gmail.com>
2026-03-10 21:03:51 -0600

    Adding modules and needed packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, EventsModule, TicketsModule, StaffModule],
   controllers: [AppController],
   providers: [AppService],
 })
